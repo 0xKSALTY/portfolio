78d8776527de0f2aa220150a0f8310165911f401
Keelan <110357096+0xKSALTY@users.noreply.github.com>
2024-06-23 18:50:16 +0100

    Added popup for About Me and functionality.
    Close button on popup not functioning, need to investigate.
    
    Need to add functionality to contact me popup.
    
    Lights and reflective elements have been added to main feature.

--- main.js
+++ main.js
@@ -14,21 +14,25 @@ renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 // shape, chosen cube
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshStandardMaterial({ color: 0xce5d0f});
+const geometry = new THREE.TorusKnotGeometry( 1.5, 0.5, 200, 16 );
+const material = new THREE.MeshStandardMaterial( { color: 0xC0C0C0, roughness: 0, metalness: 0 })
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-camera.position.z = 5;
+camera.position.z = 10;
 
 // lighting, pointlight chosen
-const pointLight = new THREE.PointLight(0xffffff, 100)
-pointLight.position.set(5,5,5)
-// const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-scene.add(pointLight) // add ambientLight
+const pointLight = new THREE.SpotLight(0xff0000, 100)
+pointLight.position.set(5,5,10)
+const pointLight2 = new THREE.PointLight(0x00dd00, 100)
+pointLight2.position.set(-5, 3, 3)
+const pointLight3 = new THREE.PointLight(0x0000ff, 100)
+pointLight3.position.set(-7, 8, 3)
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(pointLight, ambientLight, pointLight2, pointLight3) // add ambientLight
 
 // gridhelpers
 const gridHelper = new THREE.GridHelper(200, 50);
-const lightHelper = new THREE.PointLightHelper(pointLight)
+const lightHelper = new THREE.PointLightHelper(pointLight3)
 scene.add(gridHelper, lightHelper)
 
 // orbit controls 
@@ -43,10 +47,10 @@ controls.autoRotateSpeed = 0.2
 //stars in bg
 function addStar(){
 	const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-	const material = new THREE.MeshStandardMaterial({ color: 0xffffff})
+	const material = new THREE.MeshStandardMaterial( { color: 0xbbbbbb, roughness: 0, metalness: 0 })
 	const star = new THREE.Mesh( geometry, material );
 
-	const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+	const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(200));
 	star.position.set(x, y, z);
 	scene.add(star)
 }
@@ -66,34 +70,14 @@ scene.add(me);
 
 */
 
-// moon 
-const moonTexture = new THREE.TextureLoader().load('moon.jpg')
-const textureTexture = new THREE.TextureLoader().load('texture.jpg')
-
-const moon = new THREE.Mesh(
-	new THREE.SphereGeometry(3, 32, 32),
-	new THREE.MeshStandardMaterial({
-		map: moonTexture,
-		normalMap: textureTexture
-	})
-);
-scene.add(moon);
-moon.position.z = 30;
-moon.position.setX(-10);
-
 // camera movement
 function moveCamera(){
 	const t = document.body.getBoundingClientRect().top;
-	moon.rotation.x += 0.05;
-	moon.rotation.y += 0.075;
-	moon.rotation.z += 0.05;
-
 	camera.position.z = t * -0.01;
 	camera.position.x = t * -0.0002;
 	camera.position.y = y * -0.0002;
-
 }
-document.body.onscroll = moveCamera
+//document.body.onscroll = moveCamera
 
 const canvas = document.querySelector(".webgl")
 renderer.render(scene, camera)
@@ -114,25 +98,6 @@ const loop = () => {
 loop()
 
 renderer.setPixelRatio(2)
-
-/* text on screen
-const loader = new FontLoader();
-
-loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
-
-	const geometry = new TextGeometry( 'Hello three.js!', {
-		font: font,
-		size: 100,
-		depth: 5,
-		curveSegments: 12,
-		bevelEnabled: true,
-		bevelThickness: 10,
-		bevelSize: 8,
-		bevelOffset: 0,
-		bevelSegments: 5
-	} );
-} );
-*/
 
 // animation 
 function animate() {
@@ -155,26 +120,55 @@ tl.fromTo(cube.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1})
 tl.fromTo('.title', {opacity: 0}, {opacity: 1})
 nl.fromTo('nav', {y:'-100%'}, {y:'0%'})
 
-// mouse animation colour
-let mouseDown = false
-let rgb = [];
-window.addEventListener("mousedown", () => (mouseDown = true))
-window.addEventListener("mouseup", () => (mouseDown = false))
 
-window.addEventListener("mousemove", (e) => {
-	if (mouseDown){
-		rgb = [
-			Math.round((e.pageX / sizes.width) * 255), 
-			Math.round((e.pageY / sizes.height) * 255),
-			150,
-		]
-		// animate colours
-		let newColour = new THREE.Color('rgb(${rgb.join(",")})')
-		new THREE.Color('rgb(0,100, 150)')
-		gsap.to(cube.material.color, {
-			r: newColour.r, 
-			g: newColour.g, 
-			b: newColour.b,
-		})
-	}
-})
+// About Me. POPUP
+document.addEventListener('DOMContentLoaded', function() {
+
+    var aboutMeLink = document.getElementById('aboutMeLink');
+    var aboutMePopup = document.getElementById('aboutMePopup');
+    var closeBtn = document.getElementsByClassName('close')[0];
+    var aboutMeDetails = document.getElementById('aboutMeDetails');
+
+    aboutMeLink.onclick = function(event) {
+        event.preventDefault();
+        aboutMeDetails.textContent = "Hello! I'm Keelan, a Cyber Security undergrad with a passion for all things computer science! I have a military background and have been an Airtraffic Controller for nearly 6 years. I am now looking to follow my passion with all things computer science. I often use my spare time developing tools using Python and C, I have also just started to learn C++. I love the occasional CTF and playing with boxes within Hack The Box!";
+        aboutMePopup.style.display = 'block';
+    }
+
+    closeBtn.onclick = function() {
+        aboutMePopup.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        if (event.target == aboutMePopup) {
+            aboutMePopup.style.display = 'none';
+        }
+    }
+});
+
+// contact me popup add functionality here
+/*
+
+document.addEventListener('DOMContentLoaded', function() {
+    var contactMeLink = document.getElementById('contactMeLink');
+    var contactMePopup = document.getElementById('contactMePopup');
+    var closeBtn = document.getElementsByClassName('close')[0];
+    var aboutMeDetails = document.getElementById('contactMeDetails');
+
+    contactMeLink.onclick = function(event) {
+        event.preventDefault();
+        contactMeDetails.textContent = "Email: ksaltercv@gmail.com \n\n Linkedin: Keelan Salter\n Github: 0xSALTY\n";
+        contactMePopup.style.display = 'block';
+    }
+
+    closeBtn.onclick = function() {
+        contactMePopup.style.display = 'none';
+    }
+
+window.onclick = function(event) {
+        if (event.target == contactMePopup) {
+          contactMePopup.style.display = 'none';
+        }
+    }
+});
+*/
